6cf3fcb90ef0ae6edf15f97bde01a4765e95effc
RyanGAt <123759443+RyanGAt@users.noreply.github.com>
2026-08-10 14:54:43 +0100

    Proxy Vinted API during local development

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   clearScreen: false,
+  server: {
+    proxy: {
+      '/vinted-api': {
+        target: 'https://www.vinted.co.uk',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/vinted-api/, ''),
+      },
+    },
+  },
 })
